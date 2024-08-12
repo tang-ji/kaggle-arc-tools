358baed7583ae6757bd850f439224320bb061840
Ji Tang <ji.tang.jonas@outlook.com>
2024-08-12 09:13:36 +0200

    Update plot method

--- tools.py
+++ tools.py
@@ -1,38 +1,57 @@
 import matplotlib.pyplot as plt
-import matplotlib.colors as colors
-import matplotlib.ticker as ticker
-
-# Copied and modified from: https://www.kaggle.com/code/arwani/auto-encoder/notebook
-def plot(record):
-    cmap = colors.ListedColormap(
-        ['#000000', '#0074D9','#FF4136','#2ECC40','#FFDC00',
-         '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
-
-    norm = colors.Normalize(vmin=0, vmax=10)
-    def remove_tail_zeros(key, data, mode = 'inp'):
-        data = data.cpu().numpy()
-        data = data.astype(int)
-        ndata = []
-        for i in range(len(data)):
-            ndata.append(data[i] - 1 if data[i] > 0 else 0)
-        data = np.array(ndata)
-        dim = Dimension[key]['inp_dim' if mode == 'inp' else 'out_dim']
-        return data[:dim[0] * dim[1]].reshape(dim[0], dim[1])
+from matplotlib import colors
+import seaborn as sns
+def plot_task(task, task_solutions, i, t):
+    """    Plots the train and test pairs of a specified task,
+    using same color scheme as the ARC app    """    
     
-    num_rows = len(record)
-    num_cols = len(record[0])
-    fig, ax = plt.subplots(num_rows, num_cols, figsize=(15, 5))
-    for x in range(num_rows):
-        for y in range(num_cols):
-            name = list(record[x].keys())[y]
-            if num_rows > 1 and num_cols > 1:
-                sub_plot = ax[x,y]
-            elif num_rows > 1:
-                sub_plot = ax[x]
-            else:
-                sub_plot = ax[y]
-            sub_plot.imshow(record[x][name], cmap=cmap, norm=norm)
-            sub_plot.set_title(name) if x==0 else None
-            sub_plot.xaxis.set_major_locator(ticker.NullLocator())
-            sub_plot.yaxis.set_major_locator(ticker.NullLocator())
-    plt.show()
+    num_train = len(task['train'])
+    num_test  = len(task['test'])
+    
+    w=num_train+num_test
+    fig, axs  = plt.subplots(2, w, figsize=(3*w ,3*2))
+    plt.suptitle(f'Set #{i}, {t}:', fontsize=20, fontweight='bold', y=1)
+    
+    for j in range(num_train):     
+        plot_one(axs[0, j], j,'train', 'input')
+        plot_one(axs[1, j], j,'train', 'output')        
+    
+    plot_one(axs[0, j+1], 0, 'test', 'input')
+
+    cmap = colors.ListedColormap(['#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00',
+                                  '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
+    norm = colors.Normalize(vmin=0, vmax=9)
+    answer = task_solutions
+    input_matrix = answer
+    
+    axs[1, j+1].imshow(input_matrix, cmap=cmap, norm=norm)
+    axs[1, j+1].grid(True, which = 'both',color = 'lightgrey', linewidth = 0.5)
+    axs[1, j+1].set_yticks([x-0.5 for x in range(1 + len(input_matrix))])
+    axs[1, j+1].set_xticks([x-0.5 for x in range(1 + len(input_matrix[0]))])     
+    axs[1, j+1].set_xticklabels([])
+    axs[1, j+1].set_yticklabels([])
+    axs[1, j+1].set_title('TEST OUTPUT', color = 'green', fontweight='bold')
+
+    fig.patch.set_linewidth(5)
+    fig.patch.set_edgecolor('black')  # substitute 'k' for black
+    fig.patch.set_facecolor('#dddddd')
+   
+    plt.tight_layout()
+    plt.show()  
+    
+    print()
+    print()
+    
+    
+def plot_one(ax, i, train_or_test, input_or_output):
+    cmap = colors.ListedColormap(['#000000', '#0074D9', '#FF4136', '#2ECC40', '#FFDC00',
+                                  '#AAAAAA', '#F012BE', '#FF851B', '#7FDBFF', '#870C25'])
+    norm = colors.Normalize(vmin=0, vmax=9)
+    input_matrix = task[train_or_test][i][input_or_output]
+    ax.imshow(input_matrix, cmap=cmap, norm=norm)
+    ax.grid(True, which = 'both',color = 'lightgrey', linewidth = 0.5)
+    
+    plt.setp(plt.gcf().get_axes(), xticklabels=[], yticklabels=[])
+    ax.set_xticks([x-0.5 for x in range(1 + len(input_matrix[0]))])     
+    ax.set_yticks([x-0.5 for x in range(1 + len(input_matrix))])   
+    ax.set_title(train_or_test + ' ' + input_or_output, fontweight='bold')
